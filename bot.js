8864d90753324df77cd23d12d837155438ad3f26
root <tolekgames@gmail.com>
2019-05-18 21:18:09 +0200

    Pepega main file update to include new command1.js

--- bot.js
+++ bot.js
@@ -3,12 +3,15 @@
 
 
 // tmi.js lib required to run the bot
+// change the dir to whatever you wan
 //
-var tmi = require('tmi.js');
+
+var tmi = require('./tmi/node_modules/tmi.js');
 
 //bot config file with oauth and shit
 
 const config = require('./config.js');
+
 
 var client = new tmi.client(config.options);
 		// dankchars for copying [" "," "," "," ","　"]
@@ -24,6 +27,8 @@ let lastMessage = null;
 
 client.connect();
 
-const commands = require('./botmodules/commands.js')
+
+//const commands = require('./botmodules/commands.js')
+const commands1 = require('./botmodules/commands1.js')
 
 
