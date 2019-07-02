1bb831c8a0ab34064220dff50d63a9ed7ec005c6
root <tolekgames@gmail.com>
2019-07-02 14:41:44 +0200

    Edited syntax, fixed command functions. 30s cooldown workaround done

--- bot.js
+++ bot.js
@@ -15,13 +15,13 @@ const config = require('./config.js');
 
 var client = new tmi.client(config.options);
 		// dankchars for copying [" "," "," "," ","　"]
-let lastMessage = null;
+var lastMessage = null;
     function sendMsg(channel, message){
         if (message === lastMessage) {
-            message = message + "\u{E0000}";
+            message = message + "\u{E0000} ";
         }
 			lastMessage = message;
-			return message;
+			client.say(channel, message);
     }
 
 
@@ -36,79 +36,45 @@ client.on("notice", function(channel, msgid, message) {
 	
 
 client.on("chat", function(channel, user, message, self) {
-		if(message === ";gearbox") {
-			client.say(channel, ("GachiPls GEARBOX"));
-		}
-			
-	})
+	if(message.startsWith(";gearbox")) {
+		sendMsg(channel, "GachiPls GEARBOX");
+	} else if(message.startsWith(";bl3")) {
+		sendMsg(channel, "After 5 years in development, hopefully, it would have been worth the wait GabeN ");
+	} else if(message.startsWith(";ping")) {
+		sendMsg(channel, "Pong FeelsGoodMan ");
+	} else if(message.startsWith(";nam")) {
+		sendMsg(channel, "Don't NaM me " + user['display-name']);
+	} else if(message.startsWith("@tolekkBOT ty chuju")) {
+		sendMsg(channel, "Spierdalaj " + user['display-name']);
+	} else if(message.startsWith(";tolekk")) {
+		sendMsg(channel, "Sup " + user['display-name'] + " NaM ");
+	} else if(message.startsWith(";?")) {
+		sendMsg(channel, "❓ ❓ ❓  FeelsDankMan  ❓ ❓ ❓ ");
+	} else if(message.startsWith(";dank ")) {
+		sendMsg(channel, "BOOM YOU JUST GOT DANKED FeelsDankMan " + message.substr(6));
+	} else if(message.startsWith(";supinic")) {
+		sendMsg(channel, "Fix it Supinic BabyRage");
+	} else if(message.startsWith(";kappa")) {
+		sendMsg(channel, "Chat spam Kappa 123");
+	} else if(message.startsWith("tolekkbot") || message.startsWith("tolekkBOT")) {
+		sendMsg(channel, "Pinged PepeS ");
+	} else if(message.startsWith("asd")) {
+		sendMsg(channel, "FeelsDankMan Clap ");
+	}
+})
 
-	client.on("chat", function(channel, user, message, self) {
-		if(message === ";bl3") {
-			client.say(channel, "After 5 years in development, hopefully, it would have been worth the wait GabeN ");
-		}
-			
-	})
-
-
-	client.on("chat", function(channel, user, message, self) {
-		if(message === ";ping") {
-			client.say(channel, "Pong FeelsGoodMan");
-		}
-			
-	})
-
-	client.on("chat", function(channel, user, message, self) {
-		if(message === ";nam") {
-			client.say(channel, ("Don't NaM me " + user['display-name']));
-		}
-			
-	})
-
-	client.on("chat", function(channel, user, message, self) {
-		if(message === "@tolekkBOT ty chuju") {
-			client.say("tolekk", ("Spierdalaj " + user['display-name']));
-		}
-			
-	})
+client.on("subscription", function(channel, username) {
+	client.say(channel, ("!join FeelsGoodMan Clap"));
 	
-	client.on("chat", function(channel, user, message, self) {
-		if(message === ";tolekk") {
-			client.say(channel, ("Sup " + user['display-name'] + " NaM"));
-		}
-			
-	})
-	client.on("chat", function(channel, user, message, self) {
-		if(message === ";?") {
-			client.say(channel, ("❓ ❓ ❓  FeelsDankMan  ❓ ❓ ❓ "));
-		}
-			
-	})
-	client.on("chat", function(channel, user, message, self) {
-		if(message === ";dank" + message) {
-			client.say(channel, ("BOOM YOU JUST GOT DANKED FeelsDankMan"));
-
-		}
-	})
+})
 	
-	client.on("chat", function(channel, user, message, self) {
-		if(message === ";supinic") {
-			client.say(channel, ("Fix it Supinic BabyRage"));
-		}
-			
-	client.on("subscription", function(channel, username) {
-	    client.say(channel, ("!join FeelsGoodMan Clap"));
-		}
-	)
-
-	
-	client.on("timeout", function(Supinic, username, reason, duration) {
-  	 	client.say(channel, ("You just got WAYTOODANK 'ed " + user['display-name'] ));
-		}
-	)
+//	client.on("timeout", function(Supinic, username, reason, duration) {	
+//		 	client.say(channel, ("You just got WAYTOODANK 'ed " + user['display-name']));
+//	})
 //	client.on("chat", function(channel, user, message, self) {
 //		if(message === ";eval " + function(eval) ) {
 //			client.say(channel, (user['display-name'] + " , " + "WutFace can't do it" ));
 //		}
 			
-	})
+//	})
 
