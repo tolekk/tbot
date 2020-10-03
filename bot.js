33f4da3485be9394ca2892eb32596f4c741ac228
tolekk <tolekgames@gmail.com>
2020-10-03 20:22:39 +0200

    Created a mods variable to check for moderator/broadcaster status Added a massping command Added a vanish commmand

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
 const { ChatClient } = require("dank-twitch-irc");
 const config = require("./config.json")
 
+const botVer = "0.2"
+
 let client = new ChatClient(config);
 
 client.on("connecting", () => console.log("Connecting to Twitch..."));
 client.on("ready", () => console.log("Successfully connected to chat"));
 client.on("close", (error) => {
-  if (error != null) {
-    console.error("Client closed due to error", error);
-  }
+    if (error != null) {
+        console.error("Client closed due to error", error);
+    }
 });
 
-client.on("PRIVMSG", async(msg) => {
-  console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
+client.on("PRIVMSG", async (msg) => {
+    console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
 });
 
 // See below for more events
@@ -20,16 +22,41 @@ client.on("PRIVMSG", async(msg) => {
 client.connect();
 client.join("tolekk");
 
-client.on("connect", async() => {
-    await client.say("tolekk", "joined chat");
+client.on("connect", async () => {
+    await client.say("tolekk", "tolekkBOT version " + botVer + " joined.");
 });
+
+
 
 // COMMANDS
 
-client.on("PRIVMSG", async(msg) => {
+client.on("PRIVMSG", async (msg) => {
+    let checkMod = msg.isMod || msg.badges.hasBroadcaster
     if (msg.messageText[0] === ";") {
-    const trunMsg = msg.messageText.slice(1)
-        if (trunMsg === "ping") 
-            await client.say("tolekk", "Pong!")  
-    }   
+        const trunMsg = msg.messageText.slice(1).split(" ");
+        if (trunMsg[0] === "ping") {
+            await client.say("tolekk", "Pong!");
+
+        } else if (trunMsg[0] === "github") {
+            await client.say("tolekk", "https://github.com/tolekk/tbot :)")
+        } else if (trunMsg[0] === "test") {
+            if (checkMod) { //xd
+                await client.say("tolekk", "KKona test succesful KKona");
+            } else {
+                await client.say("tolekk", "KKona test unsuccesful KKona");
+            }
+
+        } else if (trunMsg[0] === "vanish") {
+            try {
+                await client.timeout("tolekk", msg.displayName, 1, "vanish command usage");
+            } catch (error) {
+            }
+        } else if (trunMsg[0] === "pingmods") {
+            if (checkMod) {
+            let mods = await client.getMods(trunMsg[1])
+            await client.say("tolekk", "Ping 🔔 " + mods);
+            }
+        }
+    }
 });
+
